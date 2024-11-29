c53b8cb3064ec4981d90efb9112dcdb797a7388f
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-11-29 07:33:35 +0300

    Added StringTuple

---
+++ src/String/StringTuple.php
@@ -0,0 +1,155 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eophantasy\Types\String;
+
+use Countable;
+use Iterator;
+
+/**
+ * A class representing a list of string values with integer indexes.
+ * 
+ * This class implements the Countable and Iterator interfaces 
+ * to provide a way to work with the tuple as with an array.
+ * It is immutable, meaning that its value cannot be changed after it is created.
+ */
+class StringTuple implements Countable, Iterator
+{
+    /**
+     * The stored items of the tuple as string values only.
+     * 
+     * @param StringValue[] $items
+     */
+    private $items;
+
+    /**
+     * Creates a new StringTuple instance.
+     * 
+     * @param StringValue ...$stringValues The string values.
+     */
+    public function __construct(StringValue ...$stringValues)
+    {
+        $this->items = $stringValues;
+    }
+
+    /**
+     * Adds strings to the tuple and returns the new tuple.
+     * 
+     * @param StringValue ...$stringValues The string to add.
+     * @return StringTuple The new tuple.
+     */
+    public function add(StringValue ...$stringValues): StringTuple
+    {
+        $items = $this->items;
+        foreach ($stringValues as $string) {
+            $items[] = $string;
+        }
+
+        return new StringTuple(...$items);
+    }
+
+    /**
+     * Searches and removes the provided string from the tuple and returns the new tuple.
+     * 
+     * @param StringValue ...$stringValues The string to remove.
+     * @return StringTuple The new tuple.
+     */
+    public function remove(StringValue ...$stringValues): StringTuple
+    {
+        $items = $this->items;
+        foreach ($items as $key => $item) {
+            foreach ($stringValues as $string) {
+                if ($item->equals($string)) {
+                    unset($items[$key]);
+                }
+            }
+        }
+
+        return new StringTuple(...$items);
+    }
+
+    /**
+     * Checks if the provided string is in the tuple.
+     * 
+     * @param StringValue $string The string to check.
+     * @return bool
+     */
+    public function has(StringValue $string): bool
+    {
+        foreach ($this->items as $item) {
+            if ($item->equals($string)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Counts the number of items in the tuple.
+     * 
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    /**
+     * Returns the current item.
+     * 
+     * @return StringValue
+     */
+    public function current(): StringValue
+    {
+        return current($this->items);
+    }
+
+    /**
+     * Returns the key of the current item.
+     * 
+     * @return int
+     */
+    public function key(): int
+    {
+        return key($this->items);
+    }
+
+    /**
+     * Moves the internal pointer to the next item.
+     * 
+     * @return void
+     */
+    public function next(): void
+    {
+        next($this->items);
+    }
+
+    /**
+     * Rewinds the internal pointer to the first item.
+     * 
+     * @return void
+     */
+    public function rewind(): void
+    {
+        reset($this->items);
+    }
+
+    /**
+     * Checks if the current item is valid.
+     * 
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return key($this->items) !== null;
+    }
+}
---
+++ tests/String/StringTupleTest.php
@@ -0,0 +1,198 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eophantasy\Types\Tests\String;
+
+use Eophantasy\Types\String\StringTuple;
+use Eophantasy\Types\String\StringValue;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A class for testing the StringTuple class.
+ * 
+ * @covers StringTuple
+ */
+class StringTupleTest extends TestCase
+{
+    /**
+     * Tests the construct method.
+     * 
+     * @return void
+     * @covers StringTuple::__construct
+     */
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(
+            StringTuple::class,
+            new StringTuple(
+                new StringValue('a'),
+                new StringValue('b'),
+                new StringValue('c')
+            )
+        );
+    }
+
+    /**
+     * Tests the has method.
+     * 
+     * @return void
+     * @covers StringTuple::has
+     */
+    public function testHas(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertTrue($tuple->has(new StringValue('a')));
+        $this->assertTrue($tuple->has(new StringValue('b')));
+        $this->assertTrue($tuple->has(new StringValue('c')));
+        $this->assertFalse($tuple->has(new StringValue('d')));
+    }
+
+    /**
+     * Tests the count method.
+     * 
+     * @return void
+     * @covers StringTuple::count
+     */
+    public function testCount(): void
+    {
+        $this->assertEquals(
+            3,
+            (new StringTuple(
+                new StringValue('a'),
+                new StringValue('b'),
+                new StringValue('c')
+            ))->count()
+        );
+    }
+
+    /**
+     * Tests the current method.
+     * 
+     * @return void
+     * @covers StringTuple::current
+     */
+    public function testCurrent(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertEquals(new StringValue('a'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('b'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('c'), $tuple->current());
+    }
+
+    /**
+     * Tests the key method.
+     * 
+     * @return void
+     * @covers StringTuple::key
+     */
+    public function testKey(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertEquals(0, $tuple->key());
+
+        $tuple->next();
+        $this->assertEquals(1, $tuple->key());
+
+        $tuple->next();
+        $this->assertEquals(2, $tuple->key());
+    }
+
+    /**
+     * Tests the next method.
+     * 
+     * @return void
+     * @covers StringTuple::next
+     */
+    public function testNext(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertEquals(new StringValue('a'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('b'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('c'), $tuple->current());
+    }
+
+    /**
+     * Tests the rewind method.
+     * 
+     * @return void
+     * @covers StringTuple::rewind
+     */
+    public function testRewind(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertEquals(new StringValue('a'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('b'), $tuple->current());
+
+        $tuple->next();
+        $this->assertEquals(new StringValue('c'), $tuple->current());
+    }
+
+    /**
+     * Tests the valid method.
+     * 
+     * @return void
+     * @covers StringTuple::valid
+     */
+    public function testValid(): void
+    {
+        $tuple = new StringTuple(
+            new StringValue('a'),
+            new StringValue('b'),
+            new StringValue('c')
+        );
+
+        $this->assertTrue($tuple->valid());
+
+        $tuple->next();
+        $this->assertTrue($tuple->valid());
+
+        $tuple->next();
+        $this->assertTrue($tuple->valid());
+
+        $tuple->next();
+        $this->assertFalse($tuple->valid());
+    }
+}
