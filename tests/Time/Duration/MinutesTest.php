2fb59772e679de4bb9fb1fe974c57fb90025e5d8
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-17 20:14:02 +0300

    Add unit tests for Minutes class: validate constructor, arithmetic operations, and time conversion methods

---
+++ tests/Time/Duration/MinutesTest.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eophantasy\Test\Time\Duration;
+
+use Eophantasy\Time\Duration\Minutes;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A class for testing the Minutes class.
+ * 
+ * @covers Eophantasy\Time\Duration\Minutes
+ */
+final class MinutesTest extends TestCase
+{
+    /**
+     * Tests the construct method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Minutes::__construct
+     * @covers Eophantasy\Time\Duration\Duration::__construct
+     */
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(
+            Minutes::class,
+            new Minutes(1)
+        );
+    }
+
+    /**
+     * Tests the add method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::add
+     */
+    public function testAdd(): void
+    {
+        $a = new Minutes(1);
+        $b = $a->add(new Minutes(1));
+
+        $this->assertNotEquals($a, $b);
+        $this->assertNotSame($a, $b);
+        $this->assertEquals(
+            2,
+            $b->minutes()
+        );
+    }
+
+
+    /**
+     * Tests the subtract method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::subtract
+     */
+    public function testSubtract(): void
+    {
+        $a = new Minutes(2);
+        $b = $a->subtract(new Minutes(1));
+
+        $this->assertNotEquals($a, $b);
+        $this->assertNotSame($a, $b);
+        $this->assertEquals(
+            1,
+            $b->minutes()
+        );
+    }
+
+    /**
+     * Tests the microseconds method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::microseconds
+     */
+    public function testMicroseconds(): void
+    {
+        $this->assertEquals(
+            1e6 * 60,
+            (new Minutes(1))->microseconds()
+        );
+    }
+
+    /**
+     * Tests the millisecondss method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::milliseconds
+     */
+    public function testMilliseconds(): void
+    {
+        $this->assertEquals(
+            60 * 1e3,
+            (new Minutes(1))->milliseconds()
+        );
+    }
+
+    /**
+     * Tests the minutes method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::minutes
+     */
+    public function testMinutes(): void
+    {
+        $this->assertEquals(
+            25,
+            (new Minutes(25))->minutes()
+        );
+    }
+
+    /**
+     * Tests the seconds method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::seconds
+     */
+    public function testSeconds(): void
+    {
+        $this->assertEquals(
+            25 * 60,
+            (new Minutes(25))->seconds()
+        );
+    }
+
+    /**
+     * Tests the hours method.
+     * 
+     * @return void
+     * @covers Eophantasy\Time\Duration\Duration::hours
+     */
+    public function testHours(): void
+    {
+        $this->assertEquals(
+            1,
+            (new Minutes(60))->hours()
+        );
+        $this->assertEquals(
+            1,
+            (new Minutes(119))->hours()
+        );
+    }
+}
